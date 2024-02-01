a48ddf4808281f2bca23f2cbf537273b06101e7c
discpline <dimoxernavse@gmail.com>
2024-02-01 12:58:10 +0200

    deploy

--- subscription_handlers.py
+++ subscription_handlers.py
@@ -1,14 +1,14 @@
 from app import cursor_2, conn, bot, conn_2
 
 
-def subscribe(message):
+def subscribe(bot, cursor, conn, message):
     user_id = message.from_user.id
     cursor_2.execute('INSERT OR REPLACE INTO subscribers (user_id) VALUES (?);', (user_id,))
     conn.commit()
     bot.send_message(user_id, 'Ви підписані на отримання сповіщень!')
 
 
-def unsubscribe(message):
+def unsubscribe(bot, cursor, conn, message):
     user_id = message.from_user.id
     cursor_2.execute('DELETE FROM subscribers WHERE user_id = ?;', (user_id,))
     conn_2.commit()
